381ea9dbef32e412eda31531cb9116407a14e584
fhdragon <wenlong.meng@gmail.com>
2013-03-26 18:00:12 +0800

    add model FileInfo

---
+++ upload/src/main/java/f/h/dragon/upload/model/FileInfo.java
@@ -0,0 +1,22 @@
+package f.h.dragon.upload.model;
+
+import java.io.Serializable;
+
+import org.apache.log4j.Logger;
+
+/**
+ * FileInfo: TODO
+ * 
+ * @author Wenlong Meng(wenlong.meng@gmail.com)
+ * @version 1.0 at Mar 26, 2013
+ * @since 1.0
+ */
+public class FileInfo implements Serializable {
+
+	//Local variables
+	/**
+	 * logger
+	 */
+	private static Logger logger = Logger.getLogger(FileInfo.class);
+
+}
